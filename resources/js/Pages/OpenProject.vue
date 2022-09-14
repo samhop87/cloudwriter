3ca9c3e71fe8ac0477f5242ad3aa29c5745d7581
Sam Hopkinson <s.hopkinson87@gmail.com>
2022-09-14 19:56:18 +0100

    Added new project path component

--- resources/js/Pages/OpenProject.vue
+++ resources/js/Pages/OpenProject.vue
@@ -2,6 +2,7 @@
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
 import {Head} from '@inertiajs/inertia-vue3';
 import ProjectTree from "@/Pages/Components/ProjectTree.vue";
+import ProjectPath from "@/Pages/Components/ProjectPath.vue";
 
 defineProps({
     project: Object,
@@ -27,6 +28,19 @@ defineProps({
                                   :depth="0"
                                   :type="'project'">
                     </project-tree>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-12">
+            <div class="bg-white">
+                <div v-if="project">
+                    <project-path :id="project.id"
+                                  :title="project.name"
+                                  :folder="project.project"
+                                  :depth="0"
+                                  :type="'project'">
+                    </project-path>
                 </div>
             </div>
         </div>
