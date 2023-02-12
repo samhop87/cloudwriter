dbe6adf46f2ab9e5afb786c557c9663b97f0e7a5
Sam Hopkinson <s.hopkinson87@gmail.com>
2023-02-12 15:14:03 +0000

    Finished themes component

--- config/cloudwriter.php
+++ config/cloudwriter.php
@@ -8,39 +8,46 @@ return [
     ],
     'story_shapes' => [
         [
+            'id' => 1,
             'name' => 'man in a hole',
             'description' => 'The main character gets into trouble and then gets out of it again, better for the experience.',
-            'image' => 'man_in_a_hole.jpg'
+            'image' => 'man_in_a_hole.jpeg'
         ],
         [
+            'id' => 2,
             'name' => 'boy meets girl',
             'description' => 'The main character gets something wonderful, loses it, then gets it back forever.',
-            'image' => 'boy_meets_girl.jpg'
+            'image' => 'boy_meets_girl.jpeg'
         ],
         [
+            'id' => 3,
             'name' => 'from bad to worse',
             'description' => 'Main character starts off poorly, gets worse with no hope of improvement',
-            'image' => 'bad_to_worse.jpg'
+            'image' => 'bad_to_worse.jpeg'
         ],
         [
+            'id' => 4,
             'name' => 'which way is up',
             'description' => 'An ambiguous story where we aren\'t sure whether developments are good or bad.',
-            'image' => 'which_way_up.jpg'
+            'image' => 'which_way_up.jpeg'
         ],
         [
+            'id' => 5,
             'name' => 'creation story',
             'description' => 'Incremental gifts from a deity and long-term happiness',
-            'image' => 'creation_story.jpg'
+            'image' => 'creation_story.jpeg'
         ],
         [
+            'id' => 6,
             'name' => 'old testament',
             'description' => 'Receives incremental gifts from a deity, but is punished and suffers a dramatic fall from grace.',
-            'image' => 'old_testament.jpg'
+            'image' => 'old_testament.jpeg'
         ],
         [
+            'id' => 7,
             'name' => 'new testament',
             'description' => 'Same as old testament, but with a happy ending.',
-            'image' => 'new_testament.jpg'
+            'image' => 'new_testament.jpeg'
         ],
     ],
 ];
--- resources/js/Pages/Wizard/Components/Themes.vue
+++ resources/js/Pages/Wizard/Components/Themes.vue
@@ -2,6 +2,14 @@
 export default {
     props: {
         shapes: Object,
+    },
+    methods: {
+        shapeImage(image) {
+            return '/images/shapes/' + image;
+        },
+        selectShape(id) {
+            this.$emit('shapeSelected', id)
+        }
     }
 }
 </script>
@@ -13,24 +21,15 @@ export default {
     <!-- Product List -->
     <section class="py-10 bg-yellow-200">
         <div class="mx-auto grid max-w-6xl  grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            <article v-for="element in shapes" class="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
+            <article @click="selectShape(element.id)" v-for="element in shapes" :key="element.id" class="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
                 <a href="#">
                     <div class="relative flex items-end overflow-hidden rounded-xl">
-                        <img :src="element.image" alt="Hotel Photo" />
+                        <img :src="shapeImage(element.image)" alt="Hotel Photo" />
                     </div>
 
                     <div class="mt-1 p-2">
                         <h2 class="text-slate-700">{{ element.name }}</h2>
                         <p class="mt-1 text-sm text-slate-400">{{ element.description }}</p>
-
-                        <div class="mt-3 flex items-end justify-between">
-                            <div class="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                                </svg>
-                                <button class="text-sm">Choose theme</button>
-                            </div>
-                        </div>
                     </div>
                 </a>
             </article>
